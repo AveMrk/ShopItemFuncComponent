149d9accd43aa5ecf7ff88e7a24920d61ca1c317
ave_mrk <zhivokinaolyga@mail.ru>
2023-09-03 21:52:40 +0300

    fixed task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import { ShopItemFunc } from "./components/ShopItemFunc";
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
-
 
 export const App = () => {
   return (
@@ -18,7 +9,16 @@ export const App = () => {
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
-        <ShopItemFunc item={item} />
+        <ShopItemFunc 
+          item = {{
+            brand: 'Tiger of Sweden',
+            title: 'Leonard coat',
+            description: 'Minimalistic coat in cotton-blend',
+            descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+            price: 399,
+            currency: '£'
+          }}
+        />
       </div>
     </div>
   )
